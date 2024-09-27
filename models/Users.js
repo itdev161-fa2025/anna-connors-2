2bc0a2eb50832e5276af4f7ab1dab0dc82889185
@Anna-Connors <connora2@gmatc.matc.edu>
2024-09-27 13:03:29 -0500

    updates

---
+++ models/Users.js
@@ -0,0 +1,23 @@
+//imports
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('user', UserSchema);
+
+//always export
+export default User;
