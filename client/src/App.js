bc3f0457a325ca279d523d5845fb813d6192380e
@Anna-Connors <connora2@gmatc.matc.edu>
2024-09-28 10:54:40 -0500

    Completed Activity 4 - completed project 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
-//import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        GoodThings
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = { data: null }
 
-export default App;
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      })
+  };
+
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            GoodThings
+          </header>
+          {this.state.data}
+        </div>
+        );
+      }
+    }
+    
+  export default App;
