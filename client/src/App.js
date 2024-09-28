391b5bbd55eb302a484bdfd5e0dee7c04a9f00e7
@Anna-Connors <connora2@gmatc.matc.edu>
2024-09-28 09:54:02 -0500

    activity4 first commit'

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        GoodThings
+      </header>
+    </div>
+  );
+}
+
+export default App;
