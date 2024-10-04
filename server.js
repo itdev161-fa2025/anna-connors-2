ecfba52ee64c49f4b9ccf0358fc3a89f2b7a3fee
@Anna-Connors <connora2@gmatc.matc.edu>
2024-10-04 16:53:58 -0500

    completed activity-5

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from 'express';  //imports express library for use of'import'
 import connectDatabase from './config/db'; //imports function
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import User from './models/User'; //creates and saves user to the Register user endpoint
+import bcrypt from 'bcryptjs'; //as User does
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 
 //create express object
@@ -30,23 +34,68 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', 
+app.post(
+    '/api/users', 
      /** validation code */
     [
         check('name', 'Please enter your name').not().isEmpty(),
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
     ],
-    (req, res) => {
+    /*mongoose queries are asynchronous, so we add async in front of the callback*/
+    async (req, res) => {
         /**Add error code */
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         } else {
-            return res.send(req.body);
-        }
-    }
-);
+            const { name, email, password } = req.body;
+            try{
+                //check if user exists
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
+                }
+
+                //create a new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password 
+                });
+
+                //encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                //save to the db and return
+                await user.save();
+
+                //Generate and return a JWT token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr' },
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token });
+                    }
+                );
+
+            } catch (error) {
+                res.status(500).send('Server error');
+            }    //end catch block
+        } //end else
+    } //end request
+); //end post
 
 //Connection listener
 const port = 5000;
