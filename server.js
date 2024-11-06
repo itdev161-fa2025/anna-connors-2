b1af564fabb229ab0ef2a828f1a52760a286873b
@Anna-Connors <connora2@gmatc.matc.edu>
2024-11-06 14:54:43 -0600

    updates

--- server.js
+++ server.js
@@ -198,6 +198,25 @@ app.get('/api/posts', auth, async (req, res) => {
     }
 });
 
+/**
+ * @route GET api/posts/:id
+ * @desc Get post
+ */
+app.get('/api/posts', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if  (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
 
 const returnToken = (user, res) => {
     const payload = {
